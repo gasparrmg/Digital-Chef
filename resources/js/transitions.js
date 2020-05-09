8abf49c43b09abf3bfd92822a9c91d3613ffc42c
marques22 <marques22@users.noreply.github.com>
2020-05-09 18:41:33 +0200

    jim-autoplay promo & menu transition
    -> bug in menu transition

--- resources/js/transitions.js
+++ resources/js/transitions.js
@@ -1,10 +1,37 @@
 $(document).ready(function() {
+
+
     $(".dish-list.homepage .dish-card a").click(function (e) { 
         e.preventDefault();
         console.log("IM HERE");
-        $(".homepage-wrapper").fadeOut(1000, function() {
+
+        $(".dish-card").animate({ 
+            top: "+=300px",
+          }, 1000 );
+          
+         $(".dish-card img").animate({ 
+            width: "60px",
+          }, 1000 );  
+
+          $(".dish-card h2").animate({ 
+            'font-size': "1.25rem",
+          }, 1000 ); 
+
+          $(".dish-list.homepage").animate({ 
+            margin: "20px 300px 0px 300px",
+          }, 1000 ); 
+
+
+  // - TRANSITION FROM MAIN TO VIDEOPAGE ---------------
+        $(".homepage-wrapper").fadeOut(1500, function() {
             //animation complete
-            $(".videopage-wrapper").fadeIn(1000);
+
+            $(".dish-card").animate({ 
+                top: "-=300px",
+              }, 1 );
+            $(".videopage-wrapper").fadeIn(1500);
         });
     });
+ // ----------------------------------------------------
+
 });

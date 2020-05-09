c32e3f6cd8213c6d42ebbc5cfcf06803ef99b7e2
marques22 <marques22@users.noreply.github.com>
2020-05-09 19:29:59 +0200

    autoplay + transition menu

--- resources/js/transitions.js
+++ resources/js/transitions.js
@@ -2,34 +2,42 @@ $(document).ready(function() {
 
 
     $(".dish-list.homepage .dish-card a").click(function (e) { 
+
         e.preventDefault();
         console.log("IM HERE");
 
-        $(".dish-card").animate({ 
-            top: "+=300px",
-          }, 1000 );
+        $(".cta-promo-video-btn").fadeOut(100);
+       
+        $(".dish-list.homepage .dish-card").css("margin","0px")
+           
+          $(".dish-list.homepage").animate({ 
+            margin: "20px 300px 0px 300px",
+          }, 1000 ); 
           
-         $(".dish-card img").animate({ 
-            width: "60px",
-          }, 1000 );  
-
           $(".dish-card h2").animate({ 
             'font-size': "1.25rem",
           }, 1000 ); 
 
-          $(".dish-list.homepage").animate({ 
-            margin: "20px 300px 0px 300px",
-          }, 1000 ); 
+          $(".dish-card img").animate({ 
+            width: "60px",
+          }, 1000 );
+
+           $(".dish-card").animate({ 
+            top: "+=300px",
+          }, { duration: 1000, queue: false });
+
+           
 
 
   // - TRANSITION FROM MAIN TO VIDEOPAGE ---------------
         $(".homepage-wrapper").fadeOut(1500, function() {
             //animation complete
 
+            
+            $(".videopage-wrapper").fadeIn(3000);
             $(".dish-card").animate({ 
                 top: "-=300px",
-              }, 1 );
-            $(".videopage-wrapper").fadeIn(1500);
+              }, 3000 );
         });
     });
  // ----------------------------------------------------

8477d891dd20aff3b3b69d4bf7cab6df61c77ebf
marques22 <marques22@users.noreply.github.com>
2020-05-17 19:21:41 +0200

    syncro meat included

--- resources/js/main-video.js
+++ resources/js/main-video.js
@@ -27,50 +27,55 @@ function stepClicks(video, timeStamps) {
     $(".img-step.six, .txt-step.six").click(function () {
         video.currentTime = timeStamps.sopa[5][0];
     });
-} else if (window.video == "vegan")  {
+
+}   
+
+  else if (window.video == "meat")  {
+
     console.log("FIRST IF " + window.video);
 
     $(".img-step.ingredients, .txt-step.ingredients").click(function () {
-        video.currentTime = timeStamps.vegan[0][0];
+        video.currentTime = timeStamps.meat[0][0];
     });
     $(".img-step.two, .txt-step.two").click(function () {
-        video.currentTime = timeStamps.vegan[1][0];
+        video.currentTime = timeStamps.meat[1][0];
     });
     $(".img-step.three, .txt-step.three").click(function () {
-        video.currentTime = timeStamps.vegan[2][0];
+        video.currentTime = timeStamps.meat[2][0];
     });
     $(".img-step.four, .txt-step.four").click(function () {
-        video.currentTime = timeStamps.vegan[3][0];
+        video.currentTime = timeStamps.meat[3][0];
     });
     $(".img-step.five, .txt-step.five").click(function () {
-        video.currentTime = timeStamps.vegan[4][0];
+        video.currentTime = timeStamps.meat[4][0];
     });
     $(".img-step.six, .txt-step.six").click(function () {
-        video.currentTime = timeStamps.vegan[5][0];
+        video.currentTime = timeStamps.meat[5][0];
     });
     $(".img-step.seven, .txt-step.seven").click(function () {
-        video.currentTime = timeStamps.vegan[6][0];
+        video.currentTime = timeStamps.meat[6][0];
     });
     $(".img-step.eight, .txt-step.eight").click(function () {
-        video.currentTime = timeStamps.vegan[7][0];
+        video.currentTime = timeStamps.meat[7][0];
     });
     $(".img-step.nine, .txt-step.nine").click(function () {
-        video.currentTime = timeStamps.vegan[8][0];
+        video.currentTime = timeStamps.meat[8][0];
     });
     $(".img-step.ten, .txt-step.ten").click(function () {
-        video.currentTime = timeStamps.vegan[9][0];
+        video.currentTime = timeStamps.meat[9][0];
     });
     $(".img-step.eleven, .txt-step.eleven").click(function () {
-        video.currentTime = timeStamps.vegan[10][0];
+        video.currentTime = timeStamps.meat[10][0];
     });
     $(".img-step.twelve, .txt-step.twelve").click(function () {
-        video.currentTime = timeStamps.vegan[11][0];
+        video.currentTime = timeStamps.meat[11][0];
     });
     $(".img-step.thirteen, .txt-step.thirteen").click(function () {
-        video.currentTime = timeStamps.vegan[12][0];
+        video.currentTime = timeStamps.meat[12][0];
     });
 
 }
+
 }
 
 function videoControls(video, videoTimestamps) {
@@ -111,7 +116,24 @@ function videoControls(video, videoTimestamps) {
                 break;
             }
         }
+     }
+      else if (window.video == "meat")  {
+        console.log("SECOND IF " + window.video);
+
+
+        for (var i = 0; i < videoTimestamps.meat.length - 1; i++) {
+            if (video.currentTime >= videoTimestamps.meat[i][0] && video.currentTime < videoTimestamps.meat[i][1]) {
+                video.currentTime = videoTimestamps.meat[i+1][0];
+                break;
+            }
+        }
     }
+    
+
+
+
+
+
     });
 
     //PREV STEP
@@ -136,6 +158,19 @@ function videoControls(video, videoTimestamps) {
         for (var i = 1; i < videoTimestamps.vegan.length; i++) {
             if (video.currentTime >= videoTimestamps.vegan[i][0] && video.currentTime < videoTimestamps.vegan[i][1]) {
                 video.currentTime = videoTimestamps.vegan[i-1][0];
+                break;
+            }
+        }
+
+    }
+    
+     else if (window.video == "meat")  {
+        console.log("THIRD IF " + window.video);
+
+
+        for (var i = 1; i < videoTimestamps.meat.length; i++) {
+            if (video.currentTime >= videoTimestamps.meat[i][0] && video.currentTime < videoTimestamps.meat[i][1]) {
+                video.currentTime = videoTimestamps.meat[i-1][0];
                 break;
             }
         }
@@ -268,6 +303,91 @@ function stepHighlight(video, videoTimestamps) {
             $(".img-step.thirteen, .txt-step.thirteen").css("opacity", "0.3");
         }
 
+    } 
+    
+    else if (window.video == "meat")  {
+        console.log("FOURTH IF " + window.video);
+        
+
+        if (video.currentTime >= videoTimestamps.meat[0][0] && video.currentTime < videoTimestamps.meat[0][1]) {
+            $(".ingredients-bubble").fadeIn(500);
+            $(".img-step.ingredients, .txt-step.ingredients").css("opacity", "1");
+        } else {
+            $(".ingredients-bubble").fadeOut(500);
+            $(".img-step.ingredients, .txt-step.ingredients").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[1][0] && video.currentTime < videoTimestamps.meat[1][1]) {
+            $(".img-step.two, .txt-step.two").css("opacity", "1");
+        } else {
+            $(".img-step.two, .txt-step.two").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[2][0] && video.currentTime < videoTimestamps.meat[2][1]) {
+            $(".img-step.three, .txt-step.three").css("opacity", "1");
+        } else {
+            $(".img-step.three, .txt-step.three").css("opacity", "0.3");   
+        }
+        if (video.currentTime >= videoTimestamps.meat[3][0] && video.currentTime < videoTimestamps.meat[3][1]) {
+            $(".img-step.four, .txt-step.four").css("opacity", "1");
+        } else {
+            $(".img-step.four, .txt-step.four").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[4][0] && video.currentTime < videoTimestamps.meat[4][1]) {
+            $(".img-step.five, .txt-step.five").css("opacity", "1");
+            $("#image-gallery-wrapper").animate({ scrollTop: "300px" });
+        } else {
+            $(".img-step.five, .txt-step.five").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[5][0] && video.currentTime < videoTimestamps.meat[5][1]) {
+            $(".img-step.six, .txt-step.six").css("opacity", "1");
+        } else {
+            $(".img-step.six, .txt-step.six").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[6][0] && video.currentTime < videoTimestamps.meat[6][1]) {
+            $(".img-step.seven, .txt-step.seven").css("opacity", "1");
+            $("#image-gallery-wrapper").animate({ scrollTop: "300px" });
+        } else {
+            $(".img-step.seven, .txt-step.seven").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[7][0] && video.currentTime < videoTimestamps.meat[7][1]) {
+            $(".img-step.eight, .txt-step.eight").css("opacity", "1");
+        } else {
+            $(".img-step.eight, .txt-step.eight").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[8][0] && video.currentTime < videoTimestamps.meat[8][1]) {
+            $(".img-step.nine, .txt-step.nine").css("opacity", "1");
+            $("#image-gallery-wrapper").animate({ scrollTop: "300px" });
+        } else {
+            $(".img-step.nine, .txt-step.nine").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[9][0] && video.currentTime < videoTimestamps.meat[9][1]) {
+            $(".img-step.ten, .txt-step.ten").css("opacity", "1");
+        } else {
+            $(".img-step.ten, .txt-step.ten").css("opacity", "0.3");
+        }
+
+        if (video.currentTime >= videoTimestamps.meat[10][0] && video.currentTime < videoTimestamps.meat[10][1]) {
+            $(".img-step.eleven, .txt-step.eleven").css("opacity", "1");
+            $("#image-gallery-wrapper").animate({ scrollTop: "300px" });
+        } else {
+            $(".img-step.eleven, .txt-step.eleven").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[11][0] && video.currentTime < videoTimestamps.meat[11][1]) {
+            $(".img-step.twelve, .txt-step.twelve").css("opacity", "1");
+        } else {
+            $(".img-step.twelve, .txt-step.twelve").css("opacity", "0.3");
+        }
+        if (video.currentTime >= videoTimestamps.meat[12][0] && video.currentTime < videoTimestamps.meat[12][1]) {
+            $(".img-step.thirteen, .txt-step.thirteen").css("opacity", "1");
+            $("#image-gallery-wrapper").animate({ scrollTop: "300px" });
+        } else {
+            $(".img-step.thirteen, .txt-step.thirteen").css("opacity", "0.3");
+        }
+
     }
+
+
+
+
+
     });
 }
